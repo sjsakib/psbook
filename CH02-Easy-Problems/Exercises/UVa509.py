544320f81600d5840e9a3e243e3304d2504a03dd
S.j. Sakib <sjsakib.bd@gmail.com>
2017-10-03 13:15:43 +0600

    added UVa 509

---
+++ CH02-Easy-Problems/Exercises/UVa509.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Problem: UVa 509 RAID!
+Status: WA :(
+"""
+case = 1
+while True:
+    try:
+        d, s, b = map(int, input().strip().split())
+    except ValueError:
+        break
+    p = (0 if input() == 'E' else 1)
+    data = []
+    for i in range(d):
+        data.append(list(input().strip()))
+
+    valid = True
+    for i in range(s*b):
+        k = 0
+        lost = -1
+        for j in range(d):
+            c = data[j][i]
+            if c == '1':
+                k ^= 1
+            if c == 'x':
+                if lost != -1:
+                    valid = False
+                    break
+                else:
+                    lost = j
+        if not valid or (lost == -1 and k != p):
+            valid = False
+            break
+        if lost != -1:
+            data[lost][i] = '0' if k == p else '1'
+
+    if not valid:
+        print("Disk set", case, "is invalid.")
+    else:
+        content = ''
+        for i in range(b):
+            for j in range(d):
+                if j != i % d:
+                    content += ''.join(data[j][i*s:(i+1)*s])
+        if len(content) % 4:
+            content += '0'*(4 - (len(content) % 4))
+        print("Disk set", case, "is valid, contents are:", hex(int(content, 2))[2:].upper())
+    case += 1
